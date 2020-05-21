c5952f14a430b06751b998b25fca8b9c57911ddf
jannis357 <jan.pac@sbg.at>
2020-05-21 21:06:31 +0200

    Deleted Matlab files because of faulty first upload

--- mp_iscat_transmission.m
+++
@@ -1,5 +0,0 @@
-function transmittedfield = mp_iscat_transmission(illum, subst,Phase,attenuation)
-% Calculates the transmitted reference field in MP-iSCAT
-
-transmittedfield.E = illum.pol*exp(1i*Phase)*attenuation;
-transmittedfield.dir = illum.dir;
